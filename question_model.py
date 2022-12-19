bb2707910fa4d4f9440f58a45f796cabc3fdedb7
Owen Campbell <112042015+oscampbellsoup@users.noreply.github.com>
2022-12-19 02:01:05 -0600

    Update question_model.py

--- question_model.py
+++ question_model.py
@@ -1,6 +1,8 @@
+# Define Question class to transfer dictionary parameters to objects in a new list
 class Question:
-
+# Initialize question objects using two parameters: question and answer
     def __init__(self, q_text, q_answer):
+# Self statements to access the question and answer attributes
         self.text = q_text
         self.answer = q_answer
 
